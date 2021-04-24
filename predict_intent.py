dab5e46aba9aab647bb172f03182ee90cb902f9a
wyu-du <duwanyu0706@163.com>
2021-04-24 19:31:48 +0800

    update

--- predict_intent.py
+++ predict_intent.py
@@ -11,7 +11,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from transformers import AutoTokenizer, AutoModelWithLMHead, pipeline, set_seed, GPT2Tokenizer, GPT2Model, GPT2LMHeadModel
-
+from torch import Tensor
 from data import Dataset
 from model import Model
 from util import save_checkpoint, ProgressMeter, AverageMeter, num_params
@@ -105,6 +105,56 @@ def predict_intent(model, tokenizer, conditioning_model, input_text, input_label
                                         )
 
         return [tokenizer.decode(s[1:]) for s in output] # 1: to delete the pad token
+    
+def calc_banned_ngram_tokens(prev_input_ids: Tensor, num_hypos: int, no_repeat_ngram_size: int, cur_len: int) -> None:
+    """Copied from fairseq for no_repeat_ngram in beam_search"""
+    if cur_len + 1 < no_repeat_ngram_size:
+        # return no banned tokens if we haven't generated no_repeat_ngram_size tokens yet
+        return [[] for _ in range(num_hypos)]
+    generated_ngrams = [{} for _ in range(num_hypos)]
+    for idx in range(num_hypos):
+        gen_tokens = prev_input_ids[idx].tolist()
+        generated_ngram = generated_ngrams[idx]
+        for ngram in zip(*[gen_tokens[i:] for i in range(no_repeat_ngram_size)]):
+            prev_ngram_tuple = tuple(ngram[:-1])
+            generated_ngram[prev_ngram_tuple] = generated_ngram.get(prev_ngram_tuple, []) + [ngram[-1]]
+
+    def _get_generated_ngrams(hypo_idx):
+        # Before decoding the next token, prevent decoding of ngrams that have already appeared
+        start_idx = cur_len + 1 - no_repeat_ngram_size
+        ngram_idx = tuple(prev_input_ids[hypo_idx, start_idx:cur_len].tolist())
+        return generated_ngrams[hypo_idx].get(ngram_idx, [])
+
+    banned_tokens = [_get_generated_ngrams(hypo_idx) for hypo_idx in range(num_hypos)]
+    return banned_tokens
+    
+def postprocess_next_token_scores(
+    scores,
+    input_ids,
+    no_repeat_ngram_size,
+    bad_words_ids,
+    cur_len,
+    min_length,
+    max_length,
+    eos_token_id,
+    repetition_penalty,
+    batch_size,
+    num_beams,
+):
+    # set eos token prob to zero if min_length is not reached
+    if eos_token_id is not None and cur_len < min_length:
+        scores[:, eos_token_id] = -float("inf")
+
+    if no_repeat_ngram_size > 0:
+        # calculate a list of banned tokens to prevent repetitively generating the same ngrams
+        num_batch_hypotheses = batch_size * num_beams
+        # from fairseq: https://github.com/pytorch/fairseq/blob/a07cb6f40480928c9e0548b737aadd36ee66ac76/fairseq/sequence_generator.py#L345
+        banned_batch_tokens = calc_banned_ngram_tokens(
+            input_ids, num_batch_hypotheses, no_repeat_ngram_size, cur_len
+        )
+        for i, banned_tokens in enumerate(banned_batch_tokens):
+            scores[i, banned_tokens] = -float("inf")
+    return scores
 
 
 # hack of code from transformers/generation_utils.py
@@ -149,7 +199,7 @@ def _generate_no_beam_search(
             outputs = model(**model_inputs, return_dict=True)
             next_token_logits = outputs.logits[:, -1, :]
 
-            scores = model.postprocess_next_token_scores(
+            scores = postprocess_next_token_scores(
                 scores=next_token_logits,
                 input_ids=input_ids,
                 no_repeat_ngram_size=no_repeat_ngram_size,
