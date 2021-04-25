358f2648d5ad7cb9a15f83377ab4dd284d804dad
wyu-du <duwanyu0706@163.com>
2021-04-25 11:43:57 +0800

    update

--- generate.sh
+++ generate.sh
@@ -2,10 +2,11 @@
 export device_id=5
 
 CUDA_VISIBLE_DEVICES=$device_id python -u evaluate_intent.py \
+--model_string ~/baseline-dialogue/src/microsoft/DialoGPT-medium_dailydialog_raw_base_2021-04-14-09-25-54/checkpoint-8946 \
 --ckpt ckpt/intent/intent_retrain_predictor/model_best.pth.tar \
 --dataset_info ckpt/intent/intent_retrain_predictor/dataset_info \
 --length_cutoff 60 \
---in_file dailydialog_data/dailydialog_gen_test.txt > intent_preds.log
+--in_file dailydialog_data/dailydialog_gen_test.txt > intent_ft_preds.log
 #--model_path ckpt/formality/marian_finetune_fisher \
 
 
