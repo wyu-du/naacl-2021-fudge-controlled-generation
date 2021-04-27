942d56eab6704cf70c5f5296a5cb2f0a10770663
wyu-du <duwanyu0706@163.com>
2021-04-27 23:11:38 +0800

    update

--- generate_bow.sh
+++ generate_bow.sh
@@ -2,7 +2,7 @@
 export device_id=4
 
 CUDA_VISIBLE_DEVICES=$device_id python -u evaluate_topic.py \
---model_string ~/baseline-dialogue/src/microsoft/DialoGPT-medium_convai2_raw_base_2021-04-23-12-16-35/checkpoint-306168 \
+--model_string ~/baseline-dialogue/src/microsoft/DialoGPT-medium_convai2_raw_base_2021-04-26-10-21-06/checkpoint-47839 \
 --ckpt ckpt/convai2/future_word_retrain_predictor/model_best.pth.tar \
 --dataset_info ckpt/convai2/future_word_retrain_predictor/dataset_info \
 --condition_file convai2_data/convai2_gen_dev.txt \
